af2932c7f92aafd9951aa63cc9008393c326f4d1
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2021-07-14 21:38:09 -0300

    Criando hanler para formatar exceções igual no spring web

---
+++ src/main/java/com/funck/webflux/exception/GlobalExceptionHandler.java
@@ -0,0 +1,73 @@
+package com.funck.webflux.exception;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+@Order(-2)
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalExceptionHandler(
+            ErrorAttributes errorAttributes,
+            WebProperties.Resources resources,
+            ApplicationContext applicationContext,
+            ServerCodecConfigurer codecConfigurer) {
+        super(errorAttributes, resources, applicationContext);
+        this.setMessageWriters(codecConfigurer.getWriters());
+    }
+
+    /**
+     * Configura quais protocolos http vão ser tratados por este controle
+     *
+     * @param errorAttributes
+     * @return
+     */
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::formatErrorResponse);
+    }
+
+    private Mono<ServerResponse> formatErrorResponse(ServerRequest request) {
+        ErrorAttributeOptions errorAttributeOptions = getErrorAttributeOptions(request);
+
+        final Map<String, Object> errorAttributes = getErrorAttributes(request, errorAttributeOptions);
+
+        int status = (int) Optional.ofNullable(errorAttributes.get("status")).orElse(500);
+
+        return ServerResponse
+                .status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorAttributes));
+    }
+
+    private ErrorAttributeOptions getErrorAttributeOptions(ServerRequest request) {
+        String query = request.uri().getQuery();
+
+        return isTraceEnabled(query) ?
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE) :
+                ErrorAttributeOptions.defaults();
+    }
+
+    private boolean isTraceEnabled(String query) {
+        return !StringUtils.isEmpty(query) && query.contains("trace");
+    }
+
+}
