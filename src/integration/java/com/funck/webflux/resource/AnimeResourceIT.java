d0c4b69b9d374aab1787205850afb6c99e36f1c4
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2021-07-17 20:06:46 -0300

    Testes de integração

--- src/integration/java/com/funck/webflux/resource/AnimeResourceIT.java
+++ src/integration/java/com/funck/webflux/resource/AnimeResourceIT.java
@@ -4,25 +4,31 @@ import com.funck.webflux.domain.Anime;
 import com.funck.webflux.repository.AnimeRepository;
 import com.funck.webflux.service.AnimeService;
 import com.funck.webflux.util.AnimeCreator;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-import reactor.test.StepVerifier;
 
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 
 @ExtendWith(SpringExtension.class)
@@ -47,6 +53,15 @@ public class AnimeResourceIT {
     void setUp() {
         BDDMockito.when(animeRepository.findAll())
                 .thenReturn(Flux.just(anime));
+
+        BDDMockito.when(animeRepository.findById(anyInt()))
+                .thenReturn(Mono.just(anime));
+
+        BDDMockito.when(animeRepository.save(AnimeCreator.createAnimeToBeSaved()))
+                .thenReturn(Mono.just(anime));
+
+        BDDMockito.when(animeRepository.delete(any(Anime.class)))
+                .thenReturn(Mono.empty());
     }
 
     @Test
@@ -78,6 +93,112 @@ public class AnimeResourceIT {
                 .expectBody()
                 .jsonPath("$.[0].id").isEqualTo(anime.getId())
                 .jsonPath("$.[0].name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("findById returns a Mono of Anime")
+    void findById_ReturnMonoOfAnime_whenSuccessful() {
+        webTestClient
+                .get()
+                .uri("/animes/{id}", 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(anime.getId())
+                .jsonPath("$.name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("findById returns Mono error when anime does not exists")
+    void findById_ReturnMonoError_whenEmptyMonoReturns() {
+        BDDMockito.when(animeRepository.findById(anyInt()))
+                .thenReturn(Mono.empty());
+
+        webTestClient
+                .get()
+                .uri("/animes/{id}", 1)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @DisplayName("Save create an anime when successful")
+    void save_CreateAnime_whenSuccessful() {
+        webTestClient
+                .post()
+                .uri("/animes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(AnimeCreator.createAnimeToBeSaved()))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(anime.getId())
+                .jsonPath("$.name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("Save return error when name is empty")
+    void save_ReturnError_whenNameIsEmpty() {
+        webTestClient
+                .post()
+                .uri("/animes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(AnimeCreator.createAnimeToBeSaved().withName("")))
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    /*@Test
+    @DisplayName("Delete removes anime when successful")
+    void delete_Remove_whenSuccessful() {
+        BDDMockito.when(animeRepository.delete(anime)).thenReturn(Mono.empty());
+
+        webTestClient
+                .delete()
+                .uri("animes/{id}", 1)
+                .exchange()
+                .expectStatus().isNoContent();
+    }*/
+
+    @Test
+    @DisplayName("Delete returns error when anime does not exists")
+    void delete_returnError_whenNotFound() {
+        BDDMockito.when(animeRepository.findById(anyInt()))
+                .thenReturn(Mono.empty());
+
+        webTestClient
+                .delete()
+                .uri("animes/{id}", 1)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @DisplayName("Update save updated anime when successful and returns empty mono")
+    void update_SaveUpdateAndReturnEmptyMono_whenSuccessful() {
+        BDDMockito.when(animeRepository.save(anime)).thenReturn(Mono.just(anime));
+
+        webTestClient
+                .put()
+                .uri("/animes/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(anime))
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    @DisplayName("Update return mono error when anime not exists")
+    void update_returnMonoError_whenNotExists() {
+        BDDMockito.when(animeRepository.findById(anyInt())).thenReturn(Mono.empty());
+
+        webTestClient
+                .put()
+                .uri("animes/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(AnimeCreator.createValidAnime()))
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
 }
